c8e977a619425c080675a9ec35a9aad5e7eb50b3
Piotr Bernatowicz <piotrbernatowicz@tutanota.com>
2018-08-04 08:32:25 +0200

    Change set state

--- components/App.js
+++ components/App.js
@@ -1,3 +1,69 @@
+// var contacts = [
+// 	{
+// 	id: 1,
+// 	firstName: 'Jan',
+// 	lastName: 'Nowak',
+// 	email: 'jan.nowak@example.com',
+// 	},
+// 	{
+// 	id: 2,
+// 	firstName: 'Adam',
+// 	lastName: 'Kowalski',
+// 	email: 'adam.kowalski@example.com',
+// 	},
+// 	{
+// 	id: 3,
+// 	firstName: 'Zbigniew',
+// 	lastName: 'Koziol',
+// 	email: 'zbigniew.koziol@example.com',
+// 	}
+// ];
+
+// var contactForm = {
+// 	firstName: '',
+// 	lastName: '',
+// 	email: ''
+// };
+
+// var App = React.createClass({
+
+// 	getInitialState: function() {
+// 		return {
+// 			contacts: contacts,
+// 			contact: contactForm,
+// 		};
+// 	},
+
+// 	addContact: function({firstName, lastName, email}) {
+// 		var newContact = {
+// 			id: this.state.contacts.length + 1,
+// 			firstName: firstName,
+// 			lastName: lastName,
+// 			email: email
+// 		};
+
+// 		this.setState(previousList => ({
+// 			newContact: previousList.contacts.push(newContact),
+// 			firstName: '',
+// 			lastName: '',
+// 			email: ''
+// 		}));
+// 	},
+
+// 	render: function() {
+// 		return (
+// 			React.createElement('div', {className: 'app'},
+// 				React.createElement(ContactForm, {
+// 					contact: contactForm,
+// 					addContact: this.addContact,
+// 				}),
+// 				React.createElement(Contacts, {items: contacts}, {})
+// 				)
+// 			);
+// 	}
+// });
+
+///////////////////testy////////////////////////
 var contacts = [
 	{
 	id: 1,
@@ -30,7 +96,7 @@ var App = React.createClass({
 	getInitialState: function() {
 		return {
 			contacts: contacts,
-			contact: contactForm
+			contact: contactForm,
 		};
 	},
 
@@ -42,12 +108,15 @@ var App = React.createClass({
 			email: email
 		};
 
-		this.setState(previousList => ({
-			newContact: previousList.contacts.push(newContact),
-			firstName: '',
-			lastName: '',
-			email: ''
-		}));
+		this.setState((prevState) => {
+			return {contacts: prevState.contacts + newContact};
+		});
+		console.log(contacts);
+		// console.log(...prevState.contacts);
+			// newContact: newContact,
+			// firstName: '',
+			// lastName: '',
+			// email: ''
 	},
 
 	render: function() {
