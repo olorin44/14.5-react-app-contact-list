0b873570039b2392fcb360271091500660228b31
Piotr Bernatowicz <piotrbernatowicz@tutanota.com>
2018-08-15 14:49:35 +0200

    Change setstate in app

--- components/App.js
+++ components/App.js
@@ -1,69 +1,3 @@
-// var contacts = [
-// 	{
-// 	id: 1,
-// 	firstName: 'Jan',
-// 	lastName: 'Nowak',
-// 	email: 'jan.nowak@example.com',
-// 	},
-// 	{
-// 	id: 2,
-// 	firstName: 'Adam',
-// 	lastName: 'Kowalski',
-// 	email: 'adam.kowalski@example.com',
-// 	},
-// 	{
-// 	id: 3,
-// 	firstName: 'Zbigniew',
-// 	lastName: 'Koziol',
-// 	email: 'zbigniew.koziol@example.com',
-// 	}
-// ];
-
-// var contactForm = {
-// 	firstName: '',
-// 	lastName: '',
-// 	email: ''
-// };
-
-// var App = React.createClass({
-
-// 	getInitialState: function() {
-// 		return {
-// 			contacts: contacts,
-// 			contact: contactForm,
-// 		};
-// 	},
-
-// 	addContact: function({firstName, lastName, email}) {
-// 		var newContact = {
-// 			id: this.state.contacts.length + 1,
-// 			firstName: firstName,
-// 			lastName: lastName,
-// 			email: email
-// 		};
-
-// 		this.setState(previousList => ({
-// 			newContact: previousList.contacts.push(newContact),
-// 			firstName: '',
-// 			lastName: '',
-// 			email: ''
-// 		}));
-// 	},
-
-// 	render: function() {
-// 		return (
-// 			React.createElement('div', {className: 'app'},
-// 				React.createElement(ContactForm, {
-// 					contact: contactForm,
-// 					addContact: this.addContact,
-// 				}),
-// 				React.createElement(Contacts, {items: contacts}, {})
-// 				)
-// 			);
-// 	}
-// });
-
-///////////////////testy////////////////////////
 var contacts = [
 	{
 	id: 1,
@@ -108,11 +42,8 @@ var App = React.createClass({
 			email: email
 		};
 
-		this.setState((prevState) => {
-			console.log(newContact);
-			console.log(prevState.contacts);
-			contacts: prevState.contacts + newContact;
-			// contacts: prevState.contacts.push(newContact);
+		this.setState({
+		contacts:[...this.state.contacts, newContact]
 		});
 	},
 
@@ -123,7 +54,7 @@ var App = React.createClass({
 					contact: contactForm,
 					addContact: this.addContact,
 				}),
-				React.createElement(Contacts, {items: contacts}, {})
+				React.createElement(Contacts, {items: this.state.contacts}, {})
 				)
 			);
 	}
